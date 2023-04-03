78c3583571cb6280977f84dff49f94c981d78197
Mitch Macdonald <35157616+mcmacdonald@users.noreply.github.com>
2023-04-03 16:45:47 +0100

    Update 04_predictions_weighted-graphs.R

--- 04_predictions_weighted-graphs.R
+++ 04_predictions_weighted-graphs.R
@@ -1,6 +1,7 @@
 
 
-# this script estimates the count dyadic regression
+# this script predicts the structure of graphs where dyads have weighted arcs ...
+
 
 # import edges -----------------------------------------------------------------
 w_phones <- utils::read.csv("https://raw.githubusercontent.com/mcmacdonald/R2-ERGM/main/montagna_phone.csv")    # phone calls by wire taps
@@ -16,12 +17,15 @@ w_summit <- weighted(w_summit)
 
 # load attributes onto phone-tap graph
 require(statnet)
-w_phones %v% 'mob'       <- montagna$familia
+w_phones %v% 'mafia'       <- montagna$familia
 w_phones %v% 'title'     <- montagna$title
 w_phones %v% 'padrino'   <- montagna$padrino
 w_phones %v% 'arrest'    <- montagna$arrest
 w_phones %v% 'informant' <- montagna$informant
 
+
+
+# see the distribution of arc weights in the graph to determine the type of distribution used
 
 
 # ------------------------------------------------------------------------------
@@ -36,11 +40,8 @@ count_null <- function(){ # ... this code provides null estimates used to calcul
     reference = ~Poisson,
     verbose = TRUE, # adjustment for low density adjacency graphs
     control = control.ergm(
-      # main.method = 'MCMLE', # 'Robbins-Monro' method, see Snijders & van Duijn (2002)
-      # MCMC.burnin = k,   # 'more is better' ... = v x 100,000
-      #  MCMC.interval = k, # 'more is better' ... = v x 100,000
-      MCMC.prop.weights = 'TNT', # '0inflated', 'Poisson'
-      seed = 20110210 # to replicate ERGM
+      MCMC.prop.weights = 'TNT', # other options include '0inflated' or 'Poisson'
+      seed = 20110210 # to replicate the results
     ),
     )
   print(summary(b))  # print results
@@ -59,27 +60,24 @@ count_update <- function(){ # ... this code provides null estimates used to calc
     sum                               + # control
     edges                             + # graph density i.e., equivalent to the intercept 
     mutual                            +
-    gwidegree(decay = 0.5, fixed = T) +
-    gwodegree(decay = 0.5, fixed = T) +
-    gwdsp(decay = 0.5, fixed = T)     + # structural equivalence
-    gwesp(decay = 0.5, fixed = T)     +
-    nodefactor('mob', levels = -1)    + # dummies that control on Family differences in mafia membership 
+    gwidegree(decay = 1.0, fixed = T) +
+    gwodegree(decay = 1.0, fixed = T) +
+    gwdsp(decay = 1.0, fixed = T)     + # structural equivalence
+    gwesp(decay = 1.0, fixed = T)     +
+    nodefactor('mafia', levels = -1)    + # dummies that control on Family differences in mafia membership 
     nodematch('title', diff = TRUE)   +
     nodefactor('arrest', levels = -1) +
     edgecov(summit)                   +
     edgecov(summit_m)                 +
-    edgecov(nostra)                   +
+    edgecov(mafias)                   +
     edgecov(family)                   , 
-    # modelling specification
+    # model specification
     response = "weight",
     reference = ~Poisson,
     verbose = TRUE, # adjustment for low density adjacency graphs
     control = control.ergm(
-      # main.method = 'MCMLE', # 'Robbins-Monro' method, see Snijders & van Duijn (2002)
-      # MCMC.burnin = k,   # 'more is better' ... = v x 100,000
-      #  MCMC.interval = k, # 'more is better' ... = v x 100,000
-      MCMC.prop.weights = 'TNT', # faster convergence when paired with 'MPLE'
-      seed = 20110210 # to replicate ERGM
+      MCMC.prop.weights = 'TNT', # other options include '0inflated' or 'Poisson'
+      seed = 20110210 # to replicate the results
     ),
     )
   print(summary(b))  # print results

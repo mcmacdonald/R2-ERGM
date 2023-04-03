da984c3f0d34450e1239d36f7d49a482ce4ef7f6
Mitch Macdonald <35157616+mcmacdonald@users.noreply.github.com>
2023-04-03 18:46:35 +0100

    Update and rename 04_predictions_weighted-graphs.R to 05_predictions_weighted-graphs.R

--- 05_predictions_weighted-graphs.R
+++ 05_predictions_weighted-graphs.R
@@ -57,20 +57,24 @@ count_update <- function(){ # ... this code provides null estimates used to calc
   set.seed(20220915)
   b <- ergm::ergm( # equation
     w_phones ~  # predict graph properties of phone calls
-    sum                               + # control
-    edges                             + # graph density i.e., equivalent to the intercept 
-    mutual                            +
-    gwidegree(decay = 1.0, fixed = T) +
-    gwodegree(decay = 1.0, fixed = T) +
-    gwdsp(decay = 1.0, fixed = T)     + # structural equivalence
-    gwesp(decay = 1.0, fixed = T)     +
-    nodefactor('mafia', levels = -1)    + # dummies that control on Family differences in mafia membership 
-    nodematch('title', diff = TRUE)   +
-    nodefactor('arrest', levels = -1) +
-    edgecov(summit)                   +
-    edgecov(summit_m)                 +
-    edgecov(mafias)                   +
-    edgecov(family)                   , 
+    sum                                 + # control on the sum of edges or the edge weights
+    edges                               + # graph density i.e., equivalent to the intercept 
+    # endogenous terms to estimate the structure of the graph-theoretic properties 
+    mutual                              + # reciprocity of arcs
+    gwidegree(decay = 1.0, fixed = T)   +
+    gwodegree(decay = 1.0, fixed = T)   +
+    gwdsp(decay = 1.0, fixed = T)       + # structural equivalence
+    gwesp(decay = 1.0, fixed = T)       +
+    nodefactor('mafia', levels = -1)    + # dummies that control on unobserved differences in mafia membership 
+    nodeifactor('title', levels = -1)   + # dummies that control on receiver effects that relate to rank or title in the mafia
+    # nodematch('title', diff = TRUE)     +
+    nodefactor('arrest', levels = -1)   +
+    edgecov(summit)                     +
+    edgecov(summit_m)                   +
+    edgecov(mafias)                     +
+    edgecov(family)                     +
+    edgecov(summit)   : edgecov(mafias) +
+    edgecov(summit_m) : edgecov(mafias) , 
     # model specification
     response = "weight",
     reference = ~Poisson,
@@ -78,7 +82,7 @@ count_update <- function(){ # ... this code provides null estimates used to calc
     control = control.ergm(
       MCMC.prop.weights = 'TNT', # other options include '0inflated' or 'Poisson'
       seed = 20110210 # to replicate the results
-    ),
+      ),
     )
   print(summary(b))  # print results
   print(confint(b, level = 0.95)) # 95% range
